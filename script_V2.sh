f2d4c47bf8e47f31a06ec350f46ae795b1aa8066
Temistocles Zwang <temis2st@gmail.com>
2022-03-06 08:14:55 -0300

    modificado dconf,anki e tweaks

--- script_V2.sh
+++ script_V2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo -e -n"\n\033[1;36mSó utilize esse script depois que o ubuntu atualizar e reiniciar\ncom apt update e apt upgrade \033[1;32mOk?"
+echo -e -n"\n\033[1;36mSó utilize esse script depois 
+que o ubuntu atualizar e reiniciar\ncom apt update e apt upgrade \033[1;32mOk?"
 read resposta
 
 
@@ -13,7 +14,6 @@ APT() {
 }
 
 
-#comando para carregar dconf load / < /media/$USER/HD500/MagicRestaurador/minhasSettings.dconf
 COPIAR_GIT_BASH(){ #bash,git
    cd ~
    cp .bashrc Desktop/
@@ -22,7 +22,9 @@ COPIAR_GIT_BASH(){ #bash,git
    cp .gitconfig Desktop/
 
 }
-COPIAR_DCONF(){ #dconf
+COPIAR_DCONF(){
+   #comando para carregar backup
+   # dconf load / < /caminho/minhasSettings.dconf
    cd ~
    dconf dump / > minhasSettings.dconf
    cp minhasSettings.dconf Desktop/
@@ -33,7 +35,12 @@ EXTENSIONS(){
    cp -r extensions/ Desktop/
 }
 
+ANKI(){
+   cd /home/temis_zwang/snap
+   cp -r anki-woodrow Desktop/
+}
 
+# download programas
 
 SNAP vlc -y 
 SNAP code --classic -y
@@ -41,6 +48,7 @@ SNAP pomotroid -y
 SNAP discord -y
 SNAP telegram-desktop -y
 SNAP gnome-system-monitor -y
+SNAP anki-woodrow
 
 APT gparted -y
 APT obs-studio -y
@@ -48,25 +56,17 @@ APT flameshot -y
 APT gnome-usage -y
 APT unrar -y
 APT google-chrome-stable -y
-
-sudo add-apt-repository universe -y && APT gnome-tweak-tool
+APT gnome-tweaks
 
 wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
 sudo add-apt-repository 'deb https://typora.io/linux ./'
 sudo apt update
 APT typora
 
-#. instalar anki, draw.io 
-# .backup anki
-# .opção de exportar e importa confi ubuntu
+
 # .configuração intelijj e vscode e github
-#salvar o .bashrc da pasta pessoal
-#salvar o gitconfig da pasta pessoal
-#retirar os y
-#colocar typora como snap
+#colocar typora como APT
 #colocar dash to dock
-#colocar tweak tool
-#colocar chrome
-#foxitReade
-#git
+#compilar código fonte foxitReader
+#instalar git
 #draw.Io ou whismical
