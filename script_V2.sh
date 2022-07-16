c53ecc226a374caf8709929874c6fd5a33c530af
Temistocles Zwang <temis2st@gmail.com>
2022-07-16 10:06:04 -0300

    ref: interface add ao scriptv2

--- script_V2.sh
+++ script_V2.sh
@@ -90,6 +90,27 @@ DOWNLOADS (){
    echo -e "\n\033[1;36mAnki precisa ser instalado manualmente em: \n ->  https://docs.ankiweb.net/platform/linux/installing.html"
 }
 
+INTERFACE () {
+   echo -e "\n1 - Criar backup locais\n2 - Restaurar configs ubuntu\
+   \n3 - Download programas ubuntu"
+   read opcao
+   if [ $opcao -eq 1 ]; then
+      BACKUPS_LOCAIS
+   elif [ $opcao -eq 2 ]; then
+      RESTAURAR_CONFIG_UBUNTU
+   elif [ $opcao -eq 3 ]; then
+      echo -e -n"\n\033[1;36mSó utilize esse script depois 
+      que o ubuntu atualizar e reiniciar\ncom apt update e apt upgrade \033[1;32mOk?"
+      read resposta
+      DOWNLOADS
+   else
+      echo -e "\n\033[1;31mOpção inválida \033[1;32mTente novamente"
+      INTERFACE
+   fi
+}
+
+INTERFACE
+
 
 # .configuração intelijj e vscode e github
 #compilar código fonte foxitReader
