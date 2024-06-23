403723d3e0dff9a4461e9225ec89c0b806795859
Temistocles Zwang <temis2st@gmail.com>
2024-06-23 17:45:04 -0300

    add

--- main script/script_V2.sh
+++ main script/script_V2.sh
@@ -137,7 +137,7 @@ DOWNLOADS (){
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install google-chrome-stable -y
+APT google-chrome-stable -y
 
 # para remover o chrome
 # sudo apt-get remove google-chrome-stable
